7bc3f2d46193fd6f0a4147fe7c45affe46c38945
wwek <licoolgo@gmail.com>
2025-10-24 15:28:00 +0800

    refactor(DRM_Kiwi): 移除独立构建相关文件和依赖
    - 删除 DRM 独立构建的详细成功总结文档
    - 移除 standalone 版本所用的 DRM.h stub 文件
    - 删除用于独立构建的 Dockerfile 配置
    - 移除 FDK-AAC 第三方库实现及相关头文件
    - 清理

--- dream-other/DRM_Kiwi/kiwi-stubs/kiwi.gen.h
+++
@@ -1,5 +0,0 @@
-// Stub kiwi.gen.h for standalone DRM build
-#pragma once
-
-// Audio sample rate definitions
-#define SND_RATE_4CH 12000  // Default 4-channel sample rate for DRM
--- dream-other/DRM_Kiwi/kiwi-stubs/sanitizer.h
+++
@@ -1,5 +0,0 @@
-// Stub sanitizer.h for standalone DRM build
-#pragma once
-
-// Sanitizer detection (not used in standalone build)
-// #define USE_ASAN
