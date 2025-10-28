373716c2a73f162967b348ed0bdd710955928537
wwek <licoolgo@gmail.com>
2025-10-28 22:34:37 +0800

    style(drm_panel): improve signal row layout and update comments
    - Change signal quality row to prioritize SNR in comment
    - Set drm-signal-row flex-wrap to wrap for better responsiveness
    - Increase gap between items in drm-signal-row for spacing improvement
    - Remove nowrap and overflow styles to allow wrapping of signal items
    - Adjust CSS gaps and margins for consistent spacing in drm signal display

--- owx/drm_panel/DrmPanel.class.js
+++ owx/drm_panel/DrmPanel.class.js
@@ -64,10 +64,10 @@ function DrmPanel(el) {
                 '<span class="drm-indicator drm-indicator-off" data-drm-ind="msc">MSC</span>' +
             '</div>' +
 
-            // 第二行：信号质量（横向显示）
+            // 第二行：信号质量（横向显示，SNR 优先）
             '<div class="drm-signal-row">' +
-                '<span class="drm-signal-item">IF: <span class="drm-value" data-drm-val="if_level">--</span> dB</span>' +
                 '<span class="drm-signal-item">SNR: <span class="drm-value drm-snr" data-drm-val="snr">--</span> dB</span>' +
+                '<span class="drm-signal-item">IF: <span class="drm-value" data-drm-val="if_level">--</span> dB</span>' +
                 '<span class="drm-signal-item">WMER/MER: <span class="drm-value" data-drm-val="wmer">--</span>/<span class="drm-value" data-drm-val="mer">--</span> dB</span>' +
                 '<span class="drm-signal-item">Doppler/Delay: <span class="drm-value" data-drm-val="doppler">--</span>/<span class="drm-value" data-drm-val="delay">--</span></span>' +
             '</div>' +
